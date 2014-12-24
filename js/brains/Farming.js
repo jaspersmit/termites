a5bfb3f495073c35c9bf93b4d2073ea79b535f0b
Luc Vlaming <lucvlaming@users.noreply.github.com>
2014-12-24 11:55:46 +0100

    * make farming bot smarter

--- js/brains/Farming.js
+++ js/brains/Farming.js
@@ -1,5 +1,8 @@
 goog.provide('tm.brains.Farming');
 
+//x, y
+var actionToLocation = [ [0,0], [-1,0], [0,-1], [1,0], [0,1], [0,0] ];
+
 tm.brains.Farming = function(perception, memory) {
     var x = perception.x();
     var y = perception.y();
@@ -10,8 +13,19 @@ tm.brains.Farming = function(perception, memory) {
 
     var tile = perception.viewTile(0, 0);
     if(x % 2 != 0 && y % 2 != 0 && tile == tm.Tiles.PLANT) { return tm.Action.EAT; }
-    if(tile == tm.Tiles.WEED) { return tm.Action.EAT; }
-    if(perception.energy() > 7000) { return tm.Action.REPRODUCE; }
-    var actionIndex = Math.floor(Math.random() * 6);
-    return this.actions[actionIndex];
+    if(perception.energy() > 1000 && tile == tm.Tiles.WEED) { return tm.Action.EAT; }
+    if(perception.energy() > 9000) { return tm.Action.REPRODUCE; }
+    var tile = perception.viewTile(0, 0);
+    if (perception.energy() < 4000) {
+        var i = 0;
+        var actionIndex = 0;
+        do {
+            actionIndex = Math.round(Math.random() * 5);
+            i++;
+        } while (i < 4 && perception.viewTile(actionToLocation[actionIndex]) == tm.Tiles.PLANT);
+        return this.actions[actionIndex];
+    } else {
+        var actionIndex = Math.round(Math.random() * 5);
+        return this.actions[actionIndex];
+    }
 };
